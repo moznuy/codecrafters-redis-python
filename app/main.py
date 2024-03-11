8e0c07b6507054b3c1ee24b623f4763381bc23db
Serhii Charykov <laammaar@gmail.com>
2024-03-11 11:18:46 +0200

    [Stage] Respond to PING

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    client, address = server_socket.accept()
+    client.sendall(b"+PONG\r\n")
+    client.close()
 
 
 if __name__ == "__main__":
