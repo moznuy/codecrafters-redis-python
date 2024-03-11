2bc3f69e7ddd8958ad720b955e993eb2f64ebfd9
Serhii Charykov <laammaar@gmail.com>
2024-03-11 11:23:16 +0200

    [Stage] Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import socket
+from typing import Any
+
+
+def serve_client(client_tuple: tuple[socket.socket, Any]):
+    client, address = client_tuple
+    data = client.recv(4096)
+    strings = data.split(b'\n')
+    for _ in strings:
+        client.sendall(b"+PONG\r\n")
+    client.close()
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, address = server_socket.accept()
-    client.sendall(b"+PONG\r\n")
-    client.close()
+    client_tuple = server_socket.accept()
+    serve_client(client_tuple)
 
 
 if __name__ == "__main__":
