57b39489ddab16f44c86eba42aac5529005d70f5
Serhii Charykov <laammaar@gmail.com>
2024-03-11 11:39:39 +0200

    [Stage] Handle concurrent clients. Event Loop. Select

--- app/main.py
+++ app/main.py
@@ -1,24 +1,37 @@
+import select
 import socket
-from typing import Any
 
 
-def serve_client(client_tuple: tuple[socket.socket, Any]):
-    client, address = client_tuple
-    while True:
-        data = client.recv(4096)
-        if not data:
-            break
-        strings = data.split(b'\r\n')
-        pings = sum(1 for string in strings if string == b'ping')
-        for _ in range(pings):
-            client.sendall(b"+PONG\r\n")
-    client.close()
+def serve_client_once(client: socket.socket) -> bool:
+    data = client.recv(4096)
+    if not data:
+        return False
+    strings = data.split(b'\r\n')
+    pings = sum(1 for string in strings if string == b'ping')
+    for _ in range(pings):
+        client.sendall(b"+PONG\r\n")
+    return True
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_tuple = server_socket.accept()
-    serve_client(client_tuple)
+    server_socket.setblocking(False)
+    client_sockets: list[socket.socket] = []
+
+    while True:
+        read_sockets = [server_socket] + client_sockets
+        read, _, _ = select.select(read_sockets, [], [], None)
+        for sock in read:
+            if sock == server_socket:
+                client, address = server_socket.accept()
+                client_sockets.append(client)
+                continue
+
+            valid = serve_client_once(sock)
+            if valid:
+                continue
+            sock.close()
+            client_sockets.remove(sock)
 
 
 if __name__ == "__main__":
